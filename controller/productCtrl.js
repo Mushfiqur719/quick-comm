bfb881d3293e4ef435dd2804ad7daa52558d09c8
Yeasin Kabir Joy <52117661+YeasinKabirJoy@users.noreply.github.com>
2023-02-17 06:45:24 +0600

    Product Filtering Sorting Limiting and Pagination

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -1,6 +1,7 @@
 const Product = require('../models/productModel')
 const asyncHandler = require('express-async-handler')
-const slugify = require("slugify")
+const slugify = require("slugify");
+const { JsonWebTokenError } = require('jsonwebtoken');
 
 const createProduct = asyncHandler(async (req,res)=>{
     try{
@@ -53,7 +54,46 @@ const getProduct = asyncHandler(async (req,res)=>{
 
 const getAllProduct = asyncHandler(async (req,res)=>{
     try{
-        const allProducts = await Product.find();
+        // FILTERING
+        const queryObj = {...req.query};
+        const excludeFields = ["page", "sort","limit", "fields"];
+        excludeFields.forEach((el)=> delete queryObj[el]);
+    
+        let queryStr = JSON.stringify(queryObj)
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match)=> `$${match}`);
+        let query = Product.find(JSON.parse(queryStr));
+
+        // SORTING
+
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(",").join(" ");
+            query = query.sort(sortBy)
+        }else{
+            query = query.sort("-createdAt")
+        }
+
+        // LIMITING THE FIELDS
+
+        if(req.query.fields){
+            const fields = req.query.fields.split(",").join(" ");
+            query = query.select(fields)
+        }else{
+            query = query.select('-__v')
+        }
+
+        // PAGINATION
+        
+        const page = req.query.page;
+        const limit = req.query.limit;
+        const skip = (page-1)*limit;
+        query = query.skip(skip).limit(limit);
+
+        if(req.query.page){
+            const productCount = await Product.countDocuments();
+            if(skip>=productCount) throw new Error('This page does not exists!');
+        }
+
+        const allProducts = await query;
         res.json(allProducts);
     }catch(error){
         throw new Error(error);
