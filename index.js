962208f4a1a67aae4b12642bc9a35a7df1898545
Mushfiqur719 <rahmanmushfiqur719@gmail.com>
2023-02-18 12:11:47 +0600

    productCategory: CRUD funcionality

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-const dbConnect = require("./config/dbConnect");
-const {notFound,errorHandler} = require("./middlewares/errorHandler.js")
-const app = express();
-const dotenv = require("dotenv").config();
-const PORT = process.env.PORT || 8000;
-const authRouter = require("./routes/authRoute");
-const productRouter = require("./routes/productRoute")
-const blogRouter = require("./routes/blogRoute")
-
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
+const dbConnect = require("./config/dbConnect");
+const { notFound,errorHandler } = require("./middlewares/errorHandler.js")
+
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+const authRouter = require("./routes/authRoute");
+const productRouter = require("./routes/productRoute");
+const blogRouter = require("./routes/blogRoute");
+const prodcategoryRouter = require("./routes/prodcategoryRoute");
+
+
 
 dbConnect();
 app.use(morgan("dev"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(cookieParser())
+app.use(cookieParser());
   
-
 app.use("/api/user", authRouter);
 app.use("/api/product", productRouter);
 app.use("/api/blog", blogRouter);
+app.use("/api/category", prodcategoryRouter);
 
 
 app.use(notFound);
